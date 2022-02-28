784d1e102acb3e1fb4dad71d54544cbf5d227150
rolando <rolando>
2022-02-28 10:41:10 +0100

    Application installable with correct icons and service worker

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,34 @@
+import { useEffect, useState } from 'react'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
+  const [deferredPrompt, setDeferredPromt] = useState(null);
+  
+  useEffect(() => {
+    console.log('SW');
+    if ('serviceWorker' in navigator) {
+      console.log('Service worker available, installing...');
+      navigator.serviceWorker
+        .register('./sw.js')
+        .then(sw => {
+          console.log('service worker registered: ', sw);
+        })
+        .catch(err => {
+          console.log('ERROR registering sw: ', err);
+        });
+    }
+    else {
+      console.log('Service worker not available, doing nothing...');
+    }
+
+    window.addEventListener('beforeinstallprompt', (event) => {
+      console.log('before install prompt triggered');
+      setDeferredPromt(event);
+      // event.preventDefault(); <-- If you don't want to show at this stage
+      return false;
+    });
+  }, []);
+
   return <Component {...pageProps} />
 }
 
